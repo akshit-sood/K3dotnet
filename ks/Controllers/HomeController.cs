61d7fc442ac6a088bf63143f12021d7ee3a8daa1
karamjeet70 <karmjeet.singh@enukesoftware.com>
2022-02-13 22:15:00 +0530

    Add project files.

---
+++ ks/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult signupcshtml()
+        {
+            return View();
+        }
+        public IActionResult student()
+        {
+            return View();
+        }
+        public IActionResult test()
+        {
+            return View();
+        }
+        public IActionResult working()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ks/Views/Home/Contactus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ks.Views.Home
+{
+    public class ContactusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
