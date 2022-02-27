ff5683ce72620674e4244d13a7ceec1fe54761c0
Akshit sood <60150400+akshit9784@users.noreply.github.com>
2022-02-27 15:42:57 +0530

    main

---
+++ ks/Controllers/JobController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ks.Controllers
+{
+    public class JobController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
